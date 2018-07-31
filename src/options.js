c2f1eda7d5a6545965e1c04d0e3fa3a1023429c7
Milo Tolboom <milotolboom@gmail.com>
2018-07-31 21:57:59 +0200

    folder setting is now persistent

--- src/options.js
+++ src/options.js
@@ -2,6 +2,10 @@
 
 let input = document.getElementById("inputFolder");
 
+chrome.storage.sync.get('folder', function(data) {
+    input.value = data.folder;
+});
+
 input.addEventListener('input', function (evt) {
     chrome.storage.sync.set({folder: this.value}, function () {
     });
